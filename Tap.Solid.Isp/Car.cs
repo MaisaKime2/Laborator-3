6f20c8195393fe95a187dfb39296e37da9493c40
MaisaKime2 <126792217+MaisaKime2@users.noreply.github.com>
2023-04-13 02:34:44 +0300

    Add files via upload

---
+++ Tap.Solid.Isp/Car.cs
@@ -0,0 +1,27 @@
+﻿namespace Tap.Solid.Isp
+{
+    internal interface IMoveable
+    {
+        void MoveBackward();
+        void MoveForward();
+    }
+
+    internal interface ISteerable
+    {
+        void SteerLeft();
+        void SteerRight();
+    }
+
+    internal class Car : IMoveable, ISteerable
+    {
+        public void MoveBackward()
+        {
+            //Move Backward
+        }
+
+        public void MoveForward()
+        {
+            //Move Forward
+        }
+
+        public

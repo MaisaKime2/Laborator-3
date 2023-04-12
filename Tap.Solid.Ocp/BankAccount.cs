3a9f32befd1da8366b1f5d7c3f8d13a41bbe4873
MaisaKime2 <126792217+MaisaKime2@users.noreply.github.com>
2023-04-13 02:33:48 +0300

    Add files via upload

---
+++ Tap.Solid.Ocp/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿namespace Tap.Solid.Ocp
+{
+    public interface ICommissionCalculator
+   {
+       decimal CalculateCommission(decimal value);
+    }
+
+    public class RegularCommissionCalculator :ICommissionCalculator
+    {
+        public decimal CalculateCommission(decimal value)
+        {
+            return value * 0.04m;
+        }
+    }
+
+    public class GoldCommissionCalculator : ICommissionCalculator
+    {
+        public decimal CalculateCommission(decimal value)
+        {
+            return value * 0.02m;
+        }
+    }
+
+    public class BankAccount
+    {
+        public decimal AccountBalance { get; private set; }
+        public AccountType AccountType { get; private set; }
+
+        public BankAccount(decimal initialBalance, AccountType accountType)
+        {
+            AccountBalance = initialBalance;
+            AccountType = accountType;
+        }
+
+        public void ExtractMoney(decimal value,ICommissionCalculator commissionCalculator)
+        {
+            var commission = commissionCalculator.CalculateCommission(value);
+            AccountBalance = AccountBalance - (value + commission);
+        }
+    }
+}

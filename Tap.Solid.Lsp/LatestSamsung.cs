27197bf8b8267221923f8164e76c7e113a5f9e27
MaisaKime2 <126792217+MaisaKime2@users.noreply.github.com>
2023-04-13 02:34:15 +0300

    Add files via upload

---
+++ Tap.Solid.Lsp/LatestSamsung.cs
@@ -0,0 +1,53 @@
+﻿namespace Tap.Solid.Lsp
+{
+    internal class LatestSamsung : MobilePhone
+    {
+        internal override void BrowseWeb(string webAddress)
+        {
+            //browse web
+        }
+
+        internal override void MakePhoneCall(string phoneNumber)
+        {
+            //make phone call
+        }
+
+        internal override void SendMessage(string address, string message)
+        {
+            if (IsValidPhoneNumber(address))
+            {
+                SendSms(address, message);
+            }
+            else if (IsValidEmailAddress(address))
+            {
+                SendEmail(address, message);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid recipient address.");
+            }
+        }
+
+        private bool IsValidPhoneNumber(string phoneNumber)
+        {
+            // check if phone number is valid
+            return true;
+        }
+
+        private bool IsValidEmailAddress(string emailAddress)
+        {
+            // check if email address is valid
+            return true;
+        }
+
+        private void SendSms(string phoneNumber, string message)
+        {
+            // send SMS
+        }
+
+        private void SendEmail(string emailAddress, string message)
+        {
+            // send email
+        }
+    }
+}
---
+++ Tap.Solid.Lsp/MobilePhone.cs
@@ -0,0 +1,11 @@
+﻿namespace Tap.Solid.Lsp
+{
+    internal abstract class MobilePhone
+    {
+        internal abstract void BrowseWeb(string webAddress);
+
+        internal abstract void MakePhoneCall(string phoneNumber);
+
+        internal abstract void SendMessage(string address, string message);
+    }
+}

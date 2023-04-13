d0c878d5be45f0c2c07ad3346c7a896c9d03f48c
MaisaKime2 <126792217+MaisaKime2@users.noreply.github.com>
2023-04-13 15:47:26 +0300

    Add files via upload

---
+++ Tap.Solid.Isp/IVehicle.cs
@@ -0,0 +1,10 @@
+﻿namespace Tap.Solid.Isp
+{
+    internal interface IVehicle
+    {
+        public void MoveForward();
+        public void MoveBackward();
+        public void SteerLeft();
+        public void SteerRight();
+    }
+}

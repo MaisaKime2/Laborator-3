b2270f61fbcdd53ea67e7c87d7c702ddd8824858
MaisaKime2 <126792217+MaisaKime2@users.noreply.github.com>
2023-04-13 15:46:48 +0300

    Add files via upload

---
+++ Tap.Solid.Lsp/LatestSamsung.cs
@@ -0,0 +1,31 @@
+﻿namespace Tap.Solid.Lsp
+{
+    internal abstract class MobilePhone
+    {
+        internal abstract void MakePhoneCall(string phoneNumber);
+        internal abstract void SendSms(string phoneNumber);
+    }
+
+    internal class LatestSamsung : MobilePhone
+    {
+        internal override void BrowseWeb(string webAddress)
+        {
+            //browse web
+        }
+
+        internal override void MakePhoneCall(string phoneNumber)
+        {
+            //make phone call
+        }
+
+        internal override void SendEmail(string emailAddress)
+        {
+            //send email
+        }
+
+        internal override void SendSms(string phoneNumber)
+        {
+            //send SMS
+        }
+    }
+}

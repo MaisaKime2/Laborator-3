18bf1803a491330c0df6a35c4d230cc34c09c942
MaisaKime2 <126792217+MaisaKime2@users.noreply.github.com>
2023-04-13 15:45:19 +0300

    Add files via upload

---
+++ Tap.Solid.Ocp/AccountType.cs
@@ -0,0 +1,9 @@
+﻿namespace Tap.Solid.Ocp
+{
+    public enum AccountType
+    {
+        Regular,
+        Gold,
+        Platinum
+    }
+}
---
+++ Tap.Solid.Ocp/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿namespace Tap.Solid.Ocp
+{
+    public class BankAccount
+    {
+        public decimal AccountBalance { get; private set; }
+        public AccountType AccountType { get; private set; }
+
+        public BankAccount(decimal initialBalance, AccountType accountType)
+        {
+            AccountBalance = initialBalance;
+            AccountType = accountType;
+        }
+
+        public class RegularCommissionCalculator 
+    {
+        public decimal CalculateCommission(decimal value)
+        {
+            return value * 0.04m;
+        }
+    }
+
+    public class GoldCommissionCalculator 
+    {
+        public decimal CalculateCommission(decimal value)
+        {
+            return value * 0.02m;
+        }
+    }
+    }
+}
